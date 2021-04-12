a4168a167e8f89d6f2d9bfe7713433142bc2ecae
Johan <johan@prodprod.se>
2021-04-12 11:59:00 +0200

    debugging

--- src-es/http/post-make/index.js
+++ src-es/http/post-make/index.js
@@ -1,7 +1,7 @@
-const { HOST, TXT_KEY_STRING } = require('@architect/shared/constants')
-const { encrypt, setPassword } = require('@architect/shared/encryption')
-const { compress } = require('@architect/shared/compression')
-const { errorPage } = require('@architect/shared/errorPage')
+import { HOST, TXT_KEY_STRING } from '@architect/shared/constants'
+import { encrypt, setPassword } from '@architect/shared/encryption'
+import { compress } from '@architect/shared/compression'
+import { errorPage } from '@architect/shared/errorPage'
 
 const getPostedData = (requestBody) => {
   if (!requestBody) {
@@ -18,41 +18,50 @@ const getPostedData = (requestBody) => {
 const escapeHtml = string => string.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
 
 exports.handler = async function http(req) {
-  console.log(req);
-  
-  const requestBody = getPostedData(req.body);
-
-  console.log('requestBody', requestBody);
-
-  if (!requestBody?.title || !requestBody?.hostUrl || !requestBody?.bodyHtml) {
-    return errorPage;
-  }
-
-  const {title, hostUrl, bodyHtml, preferedPassword} = requestBody;
-  const inputPassword = (preferedPassword && preferedPassword !== '') ? preferedPassword : null;
-  const hostname = hostUrl.replace(/(^\w+:|^)\/\//, '');
-  const compressedHtml = compress({title, bodyHtml}, hostname);
-  const password = setPassword(inputPassword);
-  const txtRecord = `"${TXT_KEY_STRING}=${escapeHtml(password)}"`;
-  const encryptedString = await encrypt(compressedHtml, hostname, password);
-  const urlBase = `http://${hostname}.${HOST}/ess`;
-  const url = `${urlBase}/${encodeURIComponent(encryptedString)}`;
-
-  console.log('url', url);
-
   return {
-    // cors: true,
-    type: 'application/json',
+    cors: true,
     headers: {
-      'Content-Type': 'application/json',
-      // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-      'set-cookie': 'fiz=buz',
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Access-Control-Allow-Methods': 'GET, POST',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept'
+      // 'set-cookie': 'fiz=buz',
     },
     statusCode: 200,
-    body: JSON.stringify({
-      urlBase,
-      url,
-      txtRecord
-    }),
+    body: 'WORKS',
   }
+
+  // const requestBody = getPostedData(req.body);
+
+  // if (!requestBody?.title || !requestBody?.hostUrl || !requestBody?.bodyHtml) {
+  //   return errorPage;
+  // }
+
+  // const {title, hostUrl, bodyHtml, preferedPassword} = requestBody;
+  // const inputPassword = (preferedPassword && preferedPassword !== '') ? preferedPassword : null;
+  // const hostname = hostUrl.replace(/(^\w+:|^)\/\//, '');
+  // const compressedHtml = compress({title, bodyHtml}, hostname);
+  // const password = setPassword(inputPassword);
+  // const txtRecord = `"${TXT_KEY_STRING}=${escapeHtml(password)}"`;
+  // const encryptedString = await encrypt(compressedHtml, hostname, password);
+  // const urlBase = `http://${hostname}.${HOST}/ess`;
+  // const url = `${urlBase}/${encodeURIComponent(encryptedString)}`;
+
+  // return {
+  //   cors: true,
+  //   type: 'application/json',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Origin': 'http://localhost:3000',
+  //     'Access-Control-Allow-Methods': 'GET, POST',
+  //     'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept'
+  //     // 'set-cookie': 'fiz=buz',
+  //   },
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     urlBase,
+  //     url,
+  //     txtRecord
+  //   }),
+  // }
 }
